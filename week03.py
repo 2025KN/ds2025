3149c0c2d55957868c9d151b62f8ba3f4f266f2b
KL <kennel0356@gmail.com>
2025-03-24 17:52:26 +0900

    0.8 set

--- week03.py
+++ week03.py
@@ -1,6 +1,6 @@
-groups = ['HOT','Seventeen', 'Black Pink', 'NJZ']
-#reatings = [1,2,4,3, 100]   #두개의 리스트 중에서 더 작은 (여기서는 groups)리스트 갯수만큼 내부 반복
-reatings = [1,2,4,3]
+cities = ['Incheon','Seoul', 'Incheon', 'Incheon', 'Gwangju'] #리스트는 중복이 가능
+cities = set(cities)    #set을 통해 중복이 사라지고, 익셔너리와 같이 중괄호에 묶여 킷값처럼 나옴
+cities.add('Incheon')   #중복값이라 set에 들어가지 않음
+cities.add('Suwon')     #새로운 값이라 들어감
 
-groups_rating = list(zip(groups, reatings))   #zip객체(튜플) 생성 후 리스트로 만듬
-print(groups_rating)
+print(cities)   #실행할 때마다 순서가 바뀜
