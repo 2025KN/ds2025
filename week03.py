4eb0fe3c6fd88fe7b571a85ee4e2b5da3a11d15f
KL <kennel0356@gmail.com>
2025-03-24 18:07:02 +0900

    1.0 inters

--- week03.py
+++ week03.py
@@ -1,22 +1,13 @@
-def duplicate_city(cities):
-    result_city = []
-    s = set()
-
-    for city in cities:
-        l1 = len(s)
-        s.add(city)
-        l2 = len(s)
-        if l1 == l2:
-            result_city.append(city)
-    return result_city
+#156page
+#l1과 l2 리스트 사이에 중복되는 값을 리스트 l3에 넣는 함수
+def inters(l1, l2):
+    l3 = []
+    for v in l1:
+        if v in l2:
+            l3.append(v)
+    return l3
 
 
-cities = ['Incheon','Seoul', 'Incheon', 'Incheon', 'Gwangju'] #리스트는 중복이 가능
-# cities = set(cities)    #set을 통해 중복이 사라지고, 익셔너리와 같이 중괄호에 묶여 킷값처럼 나옴
-# cities.add('Incheon')   #중복값이라 set에 들어가지 않음
-# cities.add('Suwon')     #새로운 값이라 들어감
-cities.append('Incheon')
-cities.append('Seoul')
-cities.append('Suwon')
-print(cities)             #실행할 때마다 순서가 바뀜
-print(set(duplicate_city(cities)))  #set을 붙임으로서 중복값을 반복되지 않게 만들었다.
+l1 = [45,5,22,31,7,19]
+l2 = [22,1,5,2,7,28,27,19,13,41]
+print(inters(l1,l2))
