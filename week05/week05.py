140fcc147c927abbfa1c83add1403248dad2d058
KL <kennel0356@gmail.com>
2025-04-07 18:35:58 +0900

    brackets

--- week05/week05.py
+++ week05/week05.py
@@ -1,18 +1,18 @@
-#print(1+2))
-
 def is_valid_parentheses(expression : str) -> bool: #type hint, (매개타입) -> 리턴타입
     stack = list()
-    for letter in expression:
-        if letter == "(":
+    brackets = {']':'[', '}':'{', ')': '('}
+
+    for letter in expression:       #문자열에서 글자 하나씩 가져와서 검사
+        if letter in brackets.values():
             stack.append(letter)
-        if letter ==")":
-            if len(stack) ==0:
-                return False        #(1+2))
-            else:
-                stack.pop()
+        if letter in brackets.keys():
+            if not stack or stack.pop() != brackets[letter]:
+                return False
+    return not stack        #stack에 값이 있는지 확인
 
-    return len(stack) == 0          #(1+2), ((3*2)/2), ((3*2/2)
 
+print(is_valid_parentheses("[({1+2)]}"))
+print(is_valid_parentheses("[({1+2})]"))
 
 print(is_valid_parentheses("(1+2))"))
 print(is_valid_parentheses("(1+2)"))
