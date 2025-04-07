521785478b142a7c11d91c909896a82fa7ac7472
KL <kennel0356@gmail.com>
2025-04-07 17:45:34 +0900

    ãadd peek method

--- week05/week05.py
+++ week05/week05.py
@@ -23,13 +23,21 @@ class Stack:
         popped_node.link = None    #링크드 리스트의 노드가 삭제될 때 남은 링크도 없애주기
         return popped_node.data
 
+    def peek(self):
+        if self.top is None:
+            return "Stack in empty!"
+        return self.top.data
+
 
 
 s1 = Stack()
 print(s1.pop())
-
+print(s1.peek())
 s1.push("Data structure")
 s1.push("Database")
 print(s1.pop())
-print(s1.pop())
-print(s1.pop())
+print(s1.peek())
+
+
+#for i in range(3):
+#    print(s1.pop())
