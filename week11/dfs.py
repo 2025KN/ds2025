066aa97518e4f20be65bf54b405eb845704e1f3d
KL <kennel0356@gmail.com>
2025-05-19 17:14:53 +0900

    create dfs

---
+++ week11/dfs.py
@@ -0,0 +1,17 @@
+graph = [
+    [0, 1, 1, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 0, 1, 1, 1, 0],
+    [0, 0, 0, 1, 0, 1, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 1, 0]
+]
+
+def dfs(g, i, visited):
+    visited[i] = True
+    print(chr(ord('A')+i), end=' ')
+
+visited_dfs = [False for _ in range(len(graph))]
+dfs(graph, 0, visited_dfs)
