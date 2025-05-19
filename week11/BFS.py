7ca779a86ed264647214abf9e285d33945d47059
KL <kennel0356@gmail.com>
2025-05-19 18:03:01 +0900

    create bfs

---
+++ week11/BFS.py
@@ -0,0 +1,29 @@
+graph = [
+    [0, 1, 1, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 0, 1, 1, 1, 0],
+    [0, 0, 0, 1, 0, 1, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 1, 0]
+]
+
+def dfs(g, i, visited):
+    visited[i] = True
+    print(chr(ord('A')+i), end=' ')
+    for j in range(len(graph)):
+        if g[i][j] == 1 and not visited[j]:
+            dfs(g,j,visited)
+
+def bfs(g, i, visited):
+    pass
+
+
+visited_dfs = [False for _ in range(len(graph))] #방문 여부 확인 배열
+visited_bfs = [0 for _ in range(len(graph))] #방문 여부 확인 배열
+
+dfs(graph, 7, visited_dfs)
+print()
+
+bfs(graph, 4, visited_bfs)
