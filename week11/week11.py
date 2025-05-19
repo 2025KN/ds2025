9ebd7081b8ba95b703dafd1a69e772431df68ae3
KL <kennel0356@gmail.com>
2025-05-19 16:48:43 +0900

    Directed Graph

--- week11/week11.py
+++ week11/week11.py
@@ -5,6 +5,7 @@ class Graph:
 
 
 G1 = Graph(4) #vertex의 개수
+G2 = Graph(4)
 
 
 # 0 == A, 1 == B, 2 == C, 3 == D
@@ -17,4 +18,14 @@ print("G1 무방향 그래프")
 for r in range(G1.size):
     for c in range(G1.size):
         print(G1.graph[r][c], end=' ')
+    print()
+
+# 0 == A, 1 == B, 2 == C, 3 == D
+G2.graph[0][1] = 1; G2.graph[0][2] = 1
+G2.graph[3][0] = 1; G2.graph[3][2] = 1
+
+print("G3 방향 그래프")
+for r in range(G2.size):
+    for c in range(G2.size):
+        print(G2.graph[r][c], end=' ')
     print()
