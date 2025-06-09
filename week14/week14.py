bcc35be719fc8f08d361c81f6adf364f70150e03
KL <kennel0356@gmail.com>
2025-06-09 17:33:24 +0900

    boj1929 -3:

--- week14/week14.py
+++ week14/week14.py
@@ -1,30 +1,19 @@
-# s, e = input().split()
-# print(type(s),type(e))
 import math
 
+def is_prime(n) -> bool:
+  if n <= 1:
+      return False
+  elif n == 2:
+      return True
+  elif n % 2 == 0:
+      return False
+  else:
+    for i in (3, int(math.sqrt(n)) + 1, 2):
+        if n % i == 0:
+          return False
+  return True
 
-# inputs = input().split()
-# t = []
-# for i in inputs :
-#     t.append(int (i))
-# print(t)
-#
-# s, e = t
-# print(s, e)
-
-def is_prime(n) -> bool: #시간복잡도 O(n)
-    if n <=1:
-        return False
-    else:
-        for i in (2, int(math.sqrt(n) + 1)):
-            if n % i == 0:
-                return False
-    return True
-
-#문자열"3 16"을 ['3', '16'] 변환 후 int형변환 >> 마지막에 언패킹하여 s,e에 저장
 s, e = map(int, input().split())
-print(s, e)
-
 for i in range(s, e+1):
     if is_prime(i):
         print(i)
